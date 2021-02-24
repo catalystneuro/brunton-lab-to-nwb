e164c44708ccb1f1b20fc2a0f4142ed3469606b5
Michael Scheid <michaelscheid2015@u.northwestern.edu>
2021-02-24 01:02:35 -0500

    Pseudo code outline for spectrogram widget
    Outlined all the steps for processing the ECoG for the spectrogram widget.

--- brunton_lab_to_nwb/brunton_widgets.py
+++ brunton_lab_to_nwb/brunton_widgets.py
@@ -1,20 +1,24 @@
 from pynwb.behavior import Position
-from ipywidgets import widgets, Layout, fixed
+from pynwb.ecephys import ElectricalSeries
+from ipywidgets import widgets, Layout
 from nwbwidgets.utils.timeseries import align_by_times, get_timeseries_tt
 from nwbwidgets.utils.widgets import interactive_output
 from ndx_events import Events
 
-import scipy
+import plotly.graph_objects as go
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-class JointPosPSTHWidget(widgets.HBox):
-    def __init__(self, events: Events, position: Position):
+class BruntonWidgetDashboard(widgets.HBox):
+    def __init__(self, events: Events,
+                 position: Position,
+                 acquisition: ElectricalSeries):
         super().__init__()
 
-        before_ft = widgets.FloatText(.5, min=0, description='before (s)', layout=Layout(width='200px'))
-        after_ft = widgets.FloatText(2., min=0, description='after (s)', layout=Layout(width='200px'))
+        before_ft = widgets.FloatText(1.5, min=0, description='before (s)', layout=Layout(width='200px'))
+        after_ft = widgets.FloatText(1.5, min=0, description='after (s)', layout=Layout(width='200px'))
         # Extract reach arm label from events, format to match key in Position
         # spatial series
         reach_arm = events.description
@@ -24,6 +28,7 @@ class JointPosPSTHWidget(widgets.HBox):
         self.spatial_series = position.spatial_series[reach_arm]
         # Store events in object
         self.events = events.timestamps[:]
+
         self.controls = dict(
             after=after_ft,
             before=before_ft,
@@ -31,13 +36,42 @@ class JointPosPSTHWidget(widgets.HBox):
 
         out_fig = interactive_output(self.trials_psth, self.controls)
 
-        self.children = [widgets.VBox([before_ft,
-                                       after_ft,
-                                       ]),
-                         out_fig
+        # self.fig = go.FigureWidget()
+        # self.ecog_psth(acquisition)
+
+        self.children = [widgets.VBox([widgets.Hbox([before_ft,
+                                                     after_ft]),
+                                       out_fig
+                                       ]
+                                      ),
+                         # widgets.Vbox([self.fig
+                         #               ]
+                         #              )
                          ]
 
-    def trials_psth(self, before=0., after=1., figsize=(12, 12)):
+    def ecog_psth(self, acquisition, before=1.5, after=1.5):
+
+        starts = self.events - before
+        stops = self.events + after
+
+        trials = align_by_times(acquisition, starts, stops)
+
+        # Discard bad ECoG segments:
+        # Compute log-transformed spectral power density for
+        # each 10 second EcoG segment and discard segments with power below 0
+        # dB or abnormally high power at 115–125 Hz (>3 SD)
+        # compared to all segments
+
+        # Compute log-transformed, time frequency spectral power
+        # with Morlet wavelets
+
+        # Baseline subtract each segment using a
+        # baseline defined as 1.5–1 seconds before each movement initiation event
+
+        # self.fig.add_trace()
+
+
+    def trials_psth(self, before=1.5, after=1.5, figsize=(12, 12)):
         """
         Trial data by event times and plot
 
@@ -78,7 +112,7 @@ class JointPosPSTHWidget(widgets.HBox):
 
     def show_psth(self, trials, ax, before, after, align_line_color=(.7, .7, .7)):
         """
-        Calculate descriptive stats (mean, sem) on trialed data and plot
+        Calculate descriptive stats (mean, std) on trialed data and plot
 
         Parameters
         ----------
@@ -110,6 +144,24 @@ class JointPosPSTHWidget(widgets.HBox):
         ax.set_ylabel('Joint Position (pixels)')
         ax.set_xlabel('time (s)')
         ax.axvline(color=align_line_color)
+
+def process_ecog(acquisition):
+
+    # Remove DC drift by subtracting the median voltage of each electrode
+
+    # Zero artifacts with absolute voltage > 50 interquartile range [IQR].
+
+    # Band-pass filter the data (1–200 Hz)
+
+    # Notch filter at 60 Hz
+
+    # Resample data to 500 Hz
+
+    # Re-referenced to common median for each grid, strip, or depth electrode group
+
+    # Remove electrodes with standard deviation (> 5 IQR) or kurtosis (> 10 IQR)
+    # compared to the median value across channels
+
 
 def compute_speed(pos, pos_tt):
     """Compute boolean of whether the speed of the animal was above a threshold
